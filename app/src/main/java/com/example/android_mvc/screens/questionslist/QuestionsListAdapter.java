0d3bdee02ae9d87a44c1bc8921c0d8fdf331dfc1
Kriti <ks@mycomputer.local>
2020-05-16 21:00:55 +0530

    Add ViewHolder to ListView

--- app/src/main/java/com/example/android_mvc/screens/questionslist/QuestionsListAdapter.java
+++ app/src/main/java/com/example/android_mvc/screens/questionslist/QuestionsListAdapter.java
@@ -27,19 +27,27 @@ public class QuestionsListAdapter extends ArrayAdapter<Question> {
         mOnQuestionClickListener = onQuestionClickListener;
     }
 
+    private static class ViewHolder {
+        private TextView mTxtTitle;
+    }
+
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         if (convertView == null) {
             convertView = LayoutInflater.from(parent.getContext())
                     .inflate(R.layout.layout_question_list_item, parent, false);
+
+            ViewHolder holder = new ViewHolder();
+            holder.mTxtTitle = convertView.findViewById(R.id.txt_title);
+            convertView.setTag(holder);
         }
 
         final Question question = getItem(position);
 
         // bind the data to views
-        TextView txtTitle = convertView.findViewById(R.id.txt_title);
-        txtTitle.setText(question.getTitle());
+        ViewHolder holder = (ViewHolder) convertView.getTag();
+        holder.mTxtTitle.setText(question.getTitle());
 
         // set listener
         convertView.setOnClickListener(new View.OnClickListener() {
